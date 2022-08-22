5f6f9ada16ef2e888c4bb7877c64d47547529362
Tensbeam <tensrrt123@gmail.com>
2022-08-22 16:39:32 +0700

    update

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const homeRouter = require('./routes/home');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.set('view engine','ejs');
+app.use('/home',homeRouter);
+
+module.exports = app;
